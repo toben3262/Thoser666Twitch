95852d77ae729b2e6c77214ec47aad8f864c563a
Steffen Brumm <steffen.brumm@beko.at>
2016-05-27 09:29:25 +0200

    T666T-8: URL changed

--- Thoser666Twitch-backend/src/main/java/db/DBConnection.java
+++ Thoser666Twitch-backend/src/main/java/db/DBConnection.java
@@ -14,7 +14,7 @@ public class DBConnection
 
     public DBConnection() throws SQLException, ClassNotFoundException {
         ds = new JdbcDataSource();
-		ds.setURL("jdbc:h2:tcp:localhost/~/t666t");
+		ds.setURL("jdbc:h2:~/t666t");
 		ds.setUser("sa");
 		ds.setPassword("sa");
 		conn = ds.getConnection();
