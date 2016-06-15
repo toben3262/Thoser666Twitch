2de6228b4267d7493d63eeb2a5c7a83423298343
Steffen Brumm <steffen.brumm@beko.at>
2016-06-15 15:28:20 +0200

    Tests updated

--- Thoser666Twitch-ui/src/test/java/com.Thoser666.utils/CryptTest.java
+++ Thoser666Twitch-ui/src/test/java/com.Thoser666.utils/CryptTest.java
@@ -1,5 +1,10 @@
 package com.Thoser666.utils;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by bek05842 on 15.06.2016.
  */
@@ -17,18 +22,19 @@ public class CryptTest
      * Test for Encrypt data
      */
     @Test
-    public void testforEncrypt
+    public void testforEncrypt()
     {
         String erg = crypt.encrypt("testosteron");
-        assertEqual(erg, "E1uVlQYBUrB7k/2D+GHrYA==");
+        assertTrue(erg.equals("E1uVlQYBUrB7k/2D+GHrYA=="));
     }
 
     /**
      * Test for decrypt data
      */
+    @Test
     public void testforDecrypt()
     {
         String erg = crypt.decrypt("jcZyVG7CAfD+FQ9gAkhbfwzRD+tpKxeq8GRh+ub/uuc=");
-        assertEqual(erg, "Das ist der Text");
+        assertTrue(erg.equals("Das ist der Text"));
     }
 }
