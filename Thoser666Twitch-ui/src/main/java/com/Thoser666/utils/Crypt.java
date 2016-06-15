f3c4c79e394a83c5f36faa4d73f4b0195fe0093b
Steffen Brumm <steffen.brumm@beko.at>
2016-06-15 12:12:45 +0200

    Encrypt added

--- Thoser666Twitch-ui/src/main/java/com/Thoser666/utils/Crypt.java
+++ Thoser666Twitch-ui/src/main/java/com/Thoser666/utils/Crypt.java
@@ -1,11 +1,15 @@
 package com.Thoser666.utils;
 
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
 import sun.misc.BASE64Decoder;
@@ -48,5 +52,51 @@ public class Crypt
         }
         // der fertige Schluessel
         secretKeySpec = new SecretKeySpec(key, "AES");
+    }
+
+    /**
+     * encrypt text
+     * @param text
+     * @return
+     */
+    public String encrypt(String text)
+    {
+        // Verschluesseln
+        Cipher cipher = null;
+        try
+        {
+            cipher = Cipher.getInstance("AES");
+        } catch (NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+        } catch (InvalidKeyException e)
+        {
+            e.printStackTrace();
+        }
+        byte[] encrypted = new byte[0];
+        try
+        {
+            encrypted = cipher.doFinal(text.getBytes());
+        } catch (IllegalBlockSizeException e)
+        {
+            e.printStackTrace();
+        } catch (BadPaddingException e)
+        {
+            e.printStackTrace();
+        }
+
+        // bytes zu Base64-String konvertieren (dient der Lesbarkeit)
+        BASE64Encoder myEncoder = new BASE64Encoder();
+        String geheim = myEncoder.encode(encrypted);
+
+        // Ergebnis
+        return geheim;
     }
 }
