07779b2e78f9a55a039ca5b48ef36cbbbe3de8ed
Steffen Brumm <steffen.brumm@beko.at>
2016-06-15 09:23:43 +0200

    Bean for Passwordchange added

---
+++ Thoser666Twitch-ui/src/main/java/com/Thoser666/beans/PasswordChange.java
@@ -0,0 +1,41 @@
+package com.Thoser666.beans;
+
+/**
+ * Created by bek05842 on 15.06.2016.
+ */
+public class PasswordChange
+{
+    String oldPwd;
+    String newPwd1;
+    String newPwd2;
+
+    public String getOldPwd()
+    {
+        return oldPwd;
+    }
+
+    public void setOldPwd(String oldPwd)
+    {
+        this.oldPwd = oldPwd;
+    }
+
+    public String getNewPwd1()
+    {
+        return newPwd1;
+    }
+
+    public void setNewPwd1(String newPwd1)
+    {
+        this.newPwd1 = newPwd1;
+    }
+
+    public String getNewPwd2()
+    {
+        return newPwd2;
+    }
+
+    public void setNewPwd2(String newPwd2)
+    {
+        this.newPwd2 = newPwd2;
+    }
+}
