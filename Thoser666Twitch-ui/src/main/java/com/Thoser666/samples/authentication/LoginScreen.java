207a28feddbb972b5e1a4ee99752139804e3e6af
Steffen Brumm <steffen.brumm@beko.at>
2016-06-21 10:47:24 +0200

    Propertyfile deleted

--- Thoser666Twitch-ui/src/main/java/com/Thoser666/samples/authentication/LoginScreen.java
+++ Thoser666Twitch-ui/src/main/java/com/Thoser666/samples/authentication/LoginScreen.java
@@ -99,7 +99,7 @@ public class LoginScreen extends CssLayout {
         loginForm.setSizeUndefined();
         loginForm.setMargin(false);
 
-        loginForm.addComponent(username = new TextField("Username", "admin"));
+        loginForm.addComponent(username = new TextField("loginscreen.username", "admin"));
         username.setWidth(15, Unit.EM);
         loginForm.addComponent(password = new PasswordField("Password"));
         password.setWidth(15, Unit.EM);
